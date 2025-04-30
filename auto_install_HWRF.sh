ef98370ddcf6850b5bee43a8a0b8260d768c73a7
denyanto <106359931+denyanto@users.noreply.github.com>
2025-04-30 13:33:44 +0700

    Create auto_install_HWRF.sh

---
+++ auto_install_HWRF.sh
@@ -0,0 +1,4 @@
+# WRFv-4.4.1 and WRFDA Configure on CentOS 8
+Configuration of WRF 4.4.1 on CenOS
+These are some installation notes taken in the process of installing WRF version 4.4.1 on a computer with CentOS 8. This tutorial is for **Group Fellowship Training Course on Numerical Weather Prediction (NWP) 26 September - 25 October 2022**
+
